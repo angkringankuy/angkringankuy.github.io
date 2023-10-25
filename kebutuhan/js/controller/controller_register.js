ba9d90b6347f06dd3246feaa64de3d97f6794fe3
MSyahidAlFajri <masyahida963@gmail.com>
2023-10-26 01:27:56 +0700

    coba sign up

---
+++ kebutuhan/js/controller/controller_register.js
@@ -0,0 +1,14 @@
+import { postWithBearer } from "https://jscroot.github.io/api/croot.js";
+import {GetDataForm} from "../config/config.js";
+import {URLPost } from "../template/template.js";
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector("form");
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        let data = GetDataForm();
+        postWithBearer(URLPost, token, data, ResponsePost)
+    });
+});
---
+++ kebutuhan/js/template/template.js
@@ -0,0 +1,2 @@
+export let URLPost = `https://asia-southeast2-rock-prism-401900.cloudfunctions.net/warmindo-insertuser`
+export let token = 'token';
