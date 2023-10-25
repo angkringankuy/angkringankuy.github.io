1c584a28e31bac62b0283813a51acb58bb1fefa2
MSyahidAlFajri <masyahida963@gmail.com>
2023-10-26 01:53:26 +0700

    coba sign up

--- kebutuhan/js/template/template.js
+++ kebutuhan/js/template/template.js
@@ -1,2 +1,2 @@
-export let URLPost = `https://asia-southeast2-rock-prism-401900.cloudfunctions.net/warmindo-insertuser`
+export let URLPost = `https://asia-southeast2-rock-prism-401900.cloudfunctions.net/warmindo-registeruser`
 export let token = 'token';
